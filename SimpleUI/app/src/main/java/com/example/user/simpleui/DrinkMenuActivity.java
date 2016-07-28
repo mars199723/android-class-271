501846ba5f927e1d088b5e2e240170ee23d1e710
mars199723 <04170218@scu.edu.tw>
2016-07-28 11:14:53 +0800

    7/28

---
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -0,0 +1,95 @@
+package com.example.user.simpleui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrinkMenuActivity extends AppCompatActivity {
+
+    ListView drinkMenuListView;
+    TextView totalTextView;
+
+    String[] drinkNames = new String[]{"White gourd tea", "Black tea", "Pearl black tea", "Milk black tea"};
+    int[] lPrices = new int[]{25,35,35,25};
+    int[] mPrices = new int[]{15,25,25,15};
+    int[] images = new int[]{R.drawable.drink1, R.drawable.drink4, R.drawable.drink3, R.drawable.drink2};
+
+    List<Drink> drinkList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_menu);
+
+        drinkMenuListView = (ListView)findViewById(R.id.drinkMenuListView);
+        totalTextView = (TextView)findViewById(R.id.totalTextView);
+
+        setData();
+
+        setupDrinkMenuListView();
+
+        Log.d("Debug", "DrinkMenuActivity OnCreate");
+    }
+
+    public void setData()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            Drink drink = new Drink();
+            drink.name = drinkNames[i];
+            drink.lPrice = lPrices[i];
+            drink.mPrice = mPrices[i];
+            drink.imageId = images[i];
+            drinkList.add(drink);
+        }
+    }
+
+    public void setupDrinkMenuListView()
+    {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("debug", "DrinkMenuActivity OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("debug", "DrinkMenuActivity OnResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("debug", "DrinkMenuActivity OnPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("debug", "DrinkMenuActivity OnStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("debug", "DrinkMenuActivity onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("debug", "DrinkMenuActivity onDestroy");
+    }
+
+}
